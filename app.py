d1e553761c7fc8b38bd7eed2ed4b0abc47166061
Tammon23 <Tammon2000@gmail.com>
2020-10-18 20:24:16 -0400

    Change log     - Added setup function that is call when the program is ran to create the tables and insert the courses     - Modified gen book to also generate a random course id     - Added function to random insert n random books into the databse (for testing)     - Added constants file which currently contains a set of all course IDs currently managed (in the future might be better to querythe courses table then save the answer)      - Unified attribute names so that the name of an attribute in the database is the same name that is in each .html      - Added BAuthor and BIsbn columns to the Books table      - Added fuctionailty to book page

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os, tool
 from flask import Flask, render_template, flash, redirect, url_for, session, request
+import mysql.connector
 
-import psycopg2, psycopg2.extras
 
 # DB connection
-#conn = psycopg2.connect(dbname='Book', user='root', password='root')
+conn = mysql.connector.connect(user='root', password='root', host="localhost", database="book")
+cur = conn.cursor(dictionary=True)
 
 app = Flask(__name__)
 
@@ -19,8 +20,8 @@ def index():
 @app.route('/index')
 def home():
     # Method should query DB for a group of books (~25, 5 rows of 5?) and then pass the information to home.html
-    books = [tool.genbook() for i in range(25)]
-
+    # books = [tool.genbook() for i in range(25)]
+    books = tool.db_get_n_books(cur, 25)
     return render_template('index.html', books=books)
 
 
@@ -28,15 +29,25 @@ def home():
 @app.route('/book/<string:isbn>')
 def book(isbn):
     # Grab info for book based on isbn
+    books = tool.db_get_n_books(cur, 25)
 
-    b = tool.genbook()
+    for b in books:
+        if b['BIsbn'] == isbn:
+            return render_template('book.html', book=b, isbn=isbn)
 
-    return render_template('book.html', book=b, isbn=isbn)
+   # if the book is not found then create a random book to show
+    newBook = tool.genbook()
+    return render_template('book.html', book=newBook, isbn=isbn)
+
+
+
 
 
 # DB search page
 @app.route('/search', methods=['GET', 'POST'])
 def search():
+    books = tool.db_get_n_books(cur, 25)
+
     # Result of search (present seperate space beneath the search space to display results, rather than redirect?)
     if request.method == 'POST':
         # Probably grab info from HTML form
@@ -48,6 +59,11 @@ def search():
 
 
 if __name__ == '__main__':
+
+    # Creating database tables and populating courses table, then adding 50 books into the Books table
+    tool.db_setup(conn, cur, "sqlcommands v1.sql")
+    tool.db_insert_random_books(conn, cur, 50)
+
     app.secret_key = os.urandom(12)
     app.debug = True
     app.run()
