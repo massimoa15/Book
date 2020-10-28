fadac01db898c4551423d0b40f1602d12d0e59fb
Dean <videkourias@gmail.com>
2020-10-28 17:16:58 -0400

    - Updated Jinja portions of book.html, search.html, and index.html - Filled out basic search function to query DB for book specified, and pass information to search.html - Updated book function to verify isbn exists, and to pull correct info from database - Localised sql queries that were previously in tool.py (tool.py should be deprecated) - Changed name of newBook.png to logo.png

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import os, tool
 from flask import Flask, render_template, flash, redirect, url_for, session, request
 import mysql.connector
-
+import random as rm
 
 # DB connection
 conn = mysql.connector.connect(user='root', password='root', host="localhost", database="book")
-cur = conn.cursor(dictionary=True)
 
 app = Flask(__name__)
 
@@ -19,51 +18,64 @@ def index():
 # Home page
 @app.route('/index')
 def home():
-    # Method should query DB for a group of books (~25, 5 rows of 5?) and then pass the information to home.html
-    # books = [tool.genbook() for i in range(25)]
-    books = tool.db_get_n_books(cur, 25)
+    cur = conn.cursor(dictionary=True)
+
+    # Fetch some number of books from DB to display
+    cur.execute("select * from books limit %s", [25])
+    books = cur.fetchall()
+
+    cur.close()
     return render_template('index.html', books=books)
 
 
 # Page for specific book
 @app.route('/book/<string:isbn>')
 def book(isbn):
+    cur = conn.cursor(dictionary=True)
+
     # Grab info for book based on isbn
-    books = tool.db_get_n_books(cur, 25)
+    cur.execute("select * from books where BIsbn = %s", [isbn])
 
-    for b in books:
-        if b['BIsbn'] == isbn:
-            return render_template('book.html', book=b, isbn=isbn)
+    # Try to fetch book info from DB
+    b = cur.fetchone()
+    cur.close()
 
-   # if the book is not found then create a random book to show
-    newBook = tool.genbook()
-    return render_template('book.html', book=newBook, isbn=isbn)
-
-
-
-
+    # Only load page if book was found
+    if b:
+        return render_template('book.html', book=b)
+    # if the book is not found then display error page
+    else:
+        return redirect(url_for('index'))
 
 # DB search page
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    books = tool.db_get_n_books(cur, 25)
+    query = "MATH-1730"
+    # Result of search
+    if request.method == 'POST' or query:
+        cur = conn.cursor(dictionary=True)
 
-    # Result of search (present seperate space beneath the search space to display results, rather than redirect?)
-    if request.method == 'POST':
-        # Probably grab info from HTML form
-        return render_template('search.html')
+        # Grab info from HTML form
+        #method = request.form.get('method')
+        #query = request.form.get('query')
+
+
+        cur.execute("select * from books where BCourseID = %s", [query])
+        books = cur.fetchall()
+
+        # Add random price
+        for b in books:
+            b['price'] = "$" + str(rm.randrange(600)) + "." + str(rm.randrange(100)).zfill(2)
+
+        cur.close()
+        return render_template('search.html', books=books, numResults=len(books))
 
     # GET method, display search and search options
     else:
-        return render_template('search.html')
+        return render_template('search.html', numResults = 0)
 
 
 if __name__ == '__main__':
-
-    # Creating database tables and populating courses table, then adding 50 books into the Books table
-    tool.db_setup(conn, cur, "sqlcommands v1.sql")
-    tool.db_insert_random_books(conn, cur, 50)
-
     app.secret_key = os.urandom(12)
     app.debug = True
     app.run()
